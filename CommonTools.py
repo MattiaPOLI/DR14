bc04f3ad9438905414e3daa558a528f356b8ebc1
MattiaPOLI <47721177+MattiaPOLI@users.noreply.github.com>
2019-03-05 22:11:20 +0100

    Update CommonTools.py

--- CommonTools.py
+++ CommonTools.py
@@ -3,6 +3,14 @@ import Plotly
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
+#colorscale used for scatterplot
+pl_colorscale = [[0.0, '#08085e'],
+                [0.333, '#08085e'],
+                [0.333, '#4ef037'],
+                [0.666, '#4ef037'],
+                [0.666, '#24bddf'],
+                [1, '#24bddf']]
+
 #function used to view the plot in google colab too; must be used before iplot
 def enable_plotly_in_cell():
   display(IPython.core.display.HTML('''<script src="/static/components/requirejs/require.js"></script>'''))
