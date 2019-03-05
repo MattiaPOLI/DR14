ed4856dca90439784411fd51710897af18e4f44c
MattiaPOLI <47721177+MattiaPOLI@users.noreply.github.com>
2019-03-05 21:17:25 +0100

    Update CommonTools.py

--- CommonTools.py
+++ CommonTools.py
@@ -1,6 +1,4 @@
 {
-  import plotly
-  
   def enable_plotly_in_cell():
     display(IPython.core.display.HTML('''<script src="/static/components/requirejs/require.js"></script>'''))
     plotly.offline.init_notebook_mode(connected=True) 
