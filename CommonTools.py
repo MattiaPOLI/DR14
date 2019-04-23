438230236171642edc3047f2e802791f9f47e27d
MattiaPOLI <47721177+MattiaPOLI@users.noreply.github.com>
2019-04-23 10:49:46 +0200

    Update CommonTools.py

--- CommonTools.py
+++ CommonTools.py
@@ -17,6 +17,10 @@ pl_colorscale = [[0.0, '#08085e'],
                 [0.666, '#24bddf'],
                 [1, '#24bddf']]
 
+#set of functions to return a specific filtered datateset
+def get_raw_dataset():
+  
+
 #function used to view the plot in google colab too; must be used before iplot
 def enable_plotly_in_cell():
   display(IPython.core.display.HTML('''<script src="/static/components/requirejs/require.js"></script>'''))
