3e4e0ae78f129053e0db710fee8958c6f477efe2
MattiaPOLI <47721177+MattiaPOLI@users.noreply.github.com>
2019-09-08 16:40:35 +0200

    Update CommonTools.py

--- CommonTools.py
+++ CommonTools.py
@@ -88,7 +88,7 @@ def variance_pca(df, graph):
 	if graph == True:
 		cum_sum = pca.explained_variance_ratio_.cumsum()
 		cum_sum = cum_sum * 100
-		bars = [go.Bar(y=cum_sum)]
+		bars = [go.Bar(y=cum_sum, x="components")]
 		enable_plotly_in_cell()
 		plotly.offline.iplot(bars, filename="cumVariance")
 	return data_PCA
