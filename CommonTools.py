fd624137d66dd41e754cd5f0996875d8d2b40f52
MattiaPOLI <47721177+MattiaPOLI@users.noreply.github.com>
2019-09-08 22:01:31 +0200

    Update CommonTools.py

--- CommonTools.py
+++ CommonTools.py
@@ -123,47 +123,6 @@ def balance_dataset(df, dfLabel, strategy="all"):
 
 # function to plot the learning curve of a classifier,with respect to the training set size
 def learning_curve_(estimator, X, y, cv=5, title="Learning Curve", n_jobs=-1, train_sizes=np.linspace(.1, 1.0, 5)):
-	"""
-	Generate a simple plot of the test and training learning curve.
-	Parameters
-	----------
-	estimator : object type that implements the "fit" and "predict" methods
-		An object of that type which is cloned for each validation.
-	title : string
-		Title for the chart.
-	X : array-like, shape (n_samples, n_features)
-		Training vector, where n_samples is the number of samples and
-		n_features is the number of features.
-	y : array-like, shape (n_samples) or (n_samples, n_features), optional
-		Target relative to X for classification or regression;
-		None for unsupervised learning.
-	cv : int, cross-validation generator or an iterable, optional
-		Determines the cross-validation splitting strategy.
-		Possible inputs for cv are:
-		  - None, to use the default 3-fold cross-validation,
-		  - integer, to specify the number of folds.
-		  - :term:`CV splitter`,
-		  - An iterable yielding (train, test) splits as arrays of indices.
-		For integer/None inputs, if ``y`` is binary or multiclass,
-		:class:`StratifiedKFold` used. If the estimator is not a classifier
-		or if ``y`` is neither binary nor multiclass, :class:`KFold` is used.
-		Refer :ref:`User Guide <cross_validation>` for the various
-		cross-validators that can be used here.
-	n_jobs : int or None, optional (default=None)
-		Number of jobs to run in parallel.
-		``None`` means 1 unless in a :obj:`joblib.parallel_backend` context.
-		``-1`` means using all processors. See :term:`Glossary <n_jobs>`
-		for more details.
-	train_sizes : array-like, shape (n_ticks,), dtype float or int
-		Relative or absolute numbers of training examples that will be used to
-		generate the learning curve. If the dtype is float, it is regarded as a
-		fraction of the maximum size of the training set (that is determined
-		by the selected validation method), i.e. it has to be within (0, 1].
-		Otherwise it is interpreted as absolute sizes of the training sets.
-		Note that for classification the number of samples usually have to
-		be big enough to contain at least one sample from each class.
-		(default: np.linspace(0.1, 1.0, 5))
-	"""
 	
 	train_sizes, train_scores, test_scores = learning_curve(
 		estimator, X, y, cv=cv, n_jobs=n_jobs, train_sizes=train_sizes, scoring="accuracy", random_state=42)
